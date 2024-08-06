9c2fcd7de6316a1dcd6f809767b3825742ee1710
Bryan Briney <bryanbriney@gmail.com>
2024-08-06 11:03:19 -0700

    filter warnings

--- balm/__init__.py
+++ balm/__init__.py
@@ -2,6 +2,12 @@
 # Distributed under the terms of the MIT License.
 # SPDX-License-Identifier: MIT
 
+import warnings
+
+warnings.filterwarnings(
+    "ignore",
+    message="Was asked to gather along dimension 0, but all input tensors were scalars; will instead unsqueeze and return a vector.",
+)
 
 from .activation import *
 from .config import *
