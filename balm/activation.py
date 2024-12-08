a8deaf2189fd69d03688b901112e8ac6ebc8d5ac
Bryan Briney <bryanbriney@gmail.com>
2024-12-08 10:51:49 -0800

    fix SwiGLU and add get_activation_fn()

--- balm/activation.py
+++ balm/activation.py
@@ -2,14 +2,60 @@
 # Distributed under the terms of the MIT License.
 # SPDX-License-Identifier: MIT
 
+from typing import Union
 
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-__all__ = ["SwiGLU"]
+__all__ = ["SwiGLU", "get_activation_fn"]
+
+
+def get_activation_fn(activation: Union[str, nn.Module]) -> nn.Module:
+    """
+    Get an activation function from a string or a PyTorch module.
+
+    Parameters
+    ----------
+    activation: Union[str, nn.Module]
+        The activation function to get. If a string, it must be one of "tanh", "gelu", "relu", or "swiglu".
+        If a module, it must be a subclass of `torch.nn.Module`, and the module will be returned as is.
+
+    Returns
+    -------
+    nn.Module
+        The activation function.
+
+    Raises
+    ------
+    ValueError
+        If the activation function is not supported.
+
+    """
+    if isinstance(activation, str):
+        activation = activation.lower()
+        if activation == "tanh":
+            return nn.Tanh()
+        elif activation == "gelu":
+            return nn.GELU()
+        elif activation == "relu":
+            return nn.ReLU()
+        elif activation == "swiglu":
+            return SwiGLU()
+        else:
+            raise ValueError(f"Unsupported activation: {activation}")
+    elif isinstance(activation, nn.Module):
+        return activation
+    raise ValueError(
+        f"Activation must be a string or a PyTorch module, got {type(activation)}"
+    )
 
 
 class SwiGLU(nn.Module):
-    def forward(self, x):
+    """
+    SwiGLU activation function.
+    """
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         x1, x2 = x.chunk(2, dim=-1)
-        return F.sigmoid(x1) * x2
+        return F.sigmoid(x2) * x1
