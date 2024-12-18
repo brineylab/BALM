dd8077572fa89fba7816ef7d751e87e07e1a1281
Bryan Briney <bryanbriney@gmail.com>
2024-12-18 14:46:49 -0800

    new tokenizer, based on PreTrainedTokenizerFast

--- balm/tokenizer.py
+++ balm/tokenizer.py
@@ -3,12 +3,13 @@
 # SPDX-License-Identifier: MIT
 
 import os
+import re
 from typing import Optional
 
-from tokenizers import Tokenizer as Tokenizer_
+from tokenizers import Regex, Tokenizer
 from tokenizers.models import WordLevel
-from tokenizers.normalizers import NFD, Sequence, StripAccents
-from tokenizers.pre_tokenizers import Whitespace
+from tokenizers.pre_tokenizers import Sequence, Split
+from tokenizers.processors import TemplateProcessing
 from transformers import PreTrainedTokenizerFast
 
 __all__ = [
@@ -16,10 +17,10 @@ __all__ = [
 ]
 
 
-class Tokenizer(PreTrainedTokenizerFast):
+class BalmTokenizer(PreTrainedTokenizerFast):
     """
     A tokenizer class mirroring the ESM-2 tokenizer's vocabulary and style, but without the <null_1> token.
-    This results in a vocabulary size of 32 (instead of ESM-2's 33).
+    This results in a vocabulary size of 32 (a `multiple of 8`_) instead of ESM-2's 33.
 
     Parameters
     ----------
@@ -42,6 +43,11 @@ class Tokenizer(PreTrainedTokenizerFast):
         Mask token. Default is "<mask>".
 
     Any additional keyword arguments are passed to the `PreTrainedTokenizerFast` constructor.
+
+
+    .. _multiple of 8:
+        https://developer.nvidia.com/blog/optimizing-gpu-performance-tensor-cores/#:~:text=Simply%20padding%20the%20vocabulary%20size,performance%20of%20the%20projection%20layer.
+
     """
 
     def __init__(
@@ -54,21 +60,51 @@ class Tokenizer(PreTrainedTokenizerFast):
         mask_token: str = "<mask>",
         **kwargs,
     ):
-        # create a WordLevel tokenizer
+        # parse vocab
         if vocab_file is not None and os.path.isfile(vocab_file):
             with open(vocab_file, "r", encoding="utf-8") as f:
                 vocab = [line.strip() for line in f if line.strip()]
         else:
             vocab = DEFAULT_VOCAB
-        tokenizer = Tokenizer_(
+        vocab_dict = {token: i for i, token in enumerate(vocab)}
+
+        # create tokenizer
+        tokenizer = Tokenizer(
             WordLevel(
-                vocab={token: i for i, token in enumerate(vocab)},
+                vocab=vocab_dict,
                 unk_token=unk_token,
             )
         )
-        # normalization and pre-tokenization
-        tokenizer.normalizer = Sequence([NFD(), StripAccents()])
-        tokenizer.pre_tokenizer = Whitespace()
+
+        # pre-tokenization
+        if "sep_token" in locals():
+            special_start_char = sep_token[0]
+            special_end_char = sep_token[-1]
+        elif "bos_token" in locals():
+            special_start_char = bos_token[0]
+            special_end_char = bos_token[-1]
+        else:
+            special_start_char = Regex("|".join(["<", "["]))
+            special_end_char = Regex("|".join([">", "]"]))
+        nonspecial_tokens = [tok for tok in vocab if len(tok) == 1]
+        pattern = "|".join([re.escape(tok) for tok in nonspecial_tokens])
+        tokenizer.pre_tokenizer = Sequence(
+            [
+                Split(special_start_char, behavior="merged_with_next"),
+                Split(special_end_char, behavior="merged_with_previous"),
+                Split(Regex(pattern), behavior="isolated"),
+            ]
+        )
+
+        # post-processing (add bos and eos tokens)
+        tokenizer.post_processor = TemplateProcessing(
+            single=f"{bos_token} $A {eos_token}",
+            pair=f"{bos_token} $A $B {eos_token}",
+            special_tokens=[
+                (bos_token, vocab_dict[bos_token]),
+                (eos_token, vocab_dict[eos_token]),
+            ],
+        )
 
         # initialize PreTrainedTokenizerFast
         super().__init__(
