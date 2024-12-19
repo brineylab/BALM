570372eb77882f6d9bef493a5585306fcb0be048
Bryan Briney <bryanbriney@gmail.com>
2024-12-18 23:21:41 -0800

    more activation options

--- balm/activation.py
+++ balm/activation.py
@@ -8,7 +8,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-__all__ = ["SwiGLU", "get_activation_fn"]
+__all__ = ["SwiGLU", "GeGLU", "ReGLU", "get_activation_fn"]
 
 
 def get_activation_fn(activation: Union[str, nn.Module]) -> nn.Module:
@@ -18,8 +18,9 @@ def get_activation_fn(activation: Union[str, nn.Module]) -> nn.Module:
     Parameters
     ----------
     activation: Union[str, nn.Module]
-        The activation function to get. If a string, it must be one of "tanh", "gelu", "relu", or "swiglu".
-        If a module, it must be a subclass of `torch.nn.Module`, and the module will be returned as is.
+        The activation function to get. If a string, it must be one of "tanh", "gelu", "relu", "glu",
+        "swiglu", "geglu", or "reglu". If a module, it must be a subclass of `torch.nn.Module`,
+        and the module will be returned as is.
 
     Returns
     -------
@@ -40,8 +41,14 @@ def get_activation_fn(activation: Union[str, nn.Module]) -> nn.Module:
             return nn.GELU()
         elif activation == "relu":
             return nn.ReLU()
+        elif activation == "glu":
+            return nn.GLU()
         elif activation == "swiglu":
             return SwiGLU()
+        elif activation == "geglu":
+            return GeGLU()
+        elif activation == "reglu":
+            return ReGLU()
         else:
             raise ValueError(f"Unsupported activation: {activation}")
     elif isinstance(activation, nn.Module):
@@ -59,3 +66,23 @@ class SwiGLU(nn.Module):
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x1, x2 = x.chunk(2, dim=-1)
         return x1 + F.silu(x2)
+
+
+class GeGLU(nn.Module):
+    """
+    GeGLU activation function.
+    """
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x1, x2 = x.chunk(2, dim=-1)
+        return x1 * F.gelu(x2)
+
+
+class ReGLU(nn.Module):
+    """
+    ReGLU activation function.
+    """
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x1, x2 = x.chunk(2, dim=-1)
+        return x1 * F.relu(x2)
